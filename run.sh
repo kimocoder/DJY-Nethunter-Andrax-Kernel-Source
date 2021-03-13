b1c0b6cabece3535c26c4faf95cde377e94aa1a7
kimocoder <christian@aircrack-ng.org>
2021-03-13 20:22:20 +0100

    Merge changes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp kali-nethunter-kernel/nethunter/Image.gz-dtb /media/kimocoder/1cb8e994-e350-457c-bf6d-51c768f325cd/kali-nethunter-project/nethunter-installer/devices/ten/oneplus6-oos/Image.gz-dtb
-rm -rf /media/kimocoder/1cb8e994-e350-457c-bf6d-51c768f325cd/kali-nethunter-project/nethunter-installer/devices/ten/oneplus6-oos/modules/system_root/lib/modules/4.9.258-NetHunter/
-cp -a kali-nethunter-kernel/nethunter/modules/system_root/lib/modules/4.9.258-NetHunter/ /media/kimocoder/1cb8e994-e350-457c-bf6d-51c768f325cd/kali-nethunter-project/nethunter-installer/devices/ten/oneplus6-oos/modules/system_root/lib/modules/
+rm -rf /media/kimocoder/1cb8e994-e350-457c-bf6d-51c768f325cd/kali-nethunter-project/nethunter-installer/devices/ten/oneplus6-oos/modules/system_root/lib/modules/*
+cp -a kali-nethunter-kernel/nethunter/modules/system_root/lib/modules/4.9.259-NetHunter/ /media/kimocoder/1cb8e994-e350-457c-bf6d-51c768f325cd/kali-nethunter-project/nethunter-installer/devices/ten/oneplus6-oos/modules/system_root/lib/modules/
 
 echo "Done!"
